1b58d19b68e103a8c070b0fdd24a09e976cde206
DanielArrivabene <danielg.arrivabene@gmail.com>
2023-10-15 18:44:34 -0300

    header position fixed

--- content/src/components/header/Navbar.jsx
+++ content/src/components/header/Navbar.jsx
@@ -2,45 +2,47 @@ import '../../styles/components/Navbar.scss';
 
 function Navbar() {
   return (
-    <nav
-      id='navbar'
-      className='navbar navbar-expand container py-4 d-flex justify-content-between'
-    >
-      <h2 className='logo'>DGA</h2>
-      <ul className='navbar-nav'>
-        <li className='nav-item'>
-          <a className='nav-link' href='#'>
-            About
-          </a>
-          <hr/>
-        </li>
-        <li className='nav-item'>
-          <a className='nav-link' href='#'>
-            About
-          </a>
-          <hr/>
-        </li>
-        <li className='nav-item'>
-          <a className='nav-link' href='#'>
-            About
-          </a>
-          <hr/>
-        </li>
-        <li className='nav-item'>
-          <a className='nav-link' href='#'>
-            teste
-          </a>
-          <hr/>
-        </li>
-        <li className='nav-item'>
-          <a className='nav-link' href='#'>
-            teste
-          </a>
-          <hr/>
-        </li>
-      </ul>
-      <button id='theme_toggler'>t</button>
-    </nav>
+    <header>
+      <nav
+        id='navbar'
+        className='navbar navbar-expand container py-4 d-flex justify-content-between'
+      >
+        <h2 className='logo'>DGA</h2>
+        <ul className='navbar-nav'>
+          <li className='nav-item'>
+            <a className='nav-link' href='#'>
+              About
+            </a>
+            <hr/>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='#'>
+              About
+            </a>
+            <hr/>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='#'>
+              About
+            </a>
+            <hr/>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='#'>
+              teste
+            </a>
+            <hr/>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='#'>
+              teste
+            </a>
+            <hr/>
+          </li>
+        </ul>
+        <button id='theme_toggler'>t</button>
+      </nav>
+    </header>
   );
 }
 
