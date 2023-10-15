171086cff14568abbb34a3675e548482dba1febb
DanielArrivabene <danielg.arrivabene@gmail.com>
2023-10-15 06:54:11 -0300

    add bootstrap

--- content/src/App.jsx
+++ content/src/App.jsx
@@ -1,11 +1,15 @@
-import './App.scss'
+import './App.scss';
+
+import Navbar from './components/header/Navbar';
 
 function App() {
-
   return (
     <>
+      <header>
+        <Navbar />
+      </header>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ content/src/components/header/Navbar.jsx
@@ -0,0 +1,26 @@
+import '../../styles/components/Navbar.scss';
+
+function Navbar() {
+  return <nav>
+    <h2 className="logo">DGA</h2>
+    <ul>
+        <li>
+            <a href="#">teste</a>
+        </li>
+        <li>
+            <a href="#">teste</a>
+        </li>
+        <li>
+            <a href="#">teste</a>
+        </li>
+        <li>
+            <a href="#">teste</a>
+        </li>
+        <li>
+            <a href="#">teste</a>
+        </li>
+    </ul>
+  </nav>;
+}
+
+export default Navbar;
