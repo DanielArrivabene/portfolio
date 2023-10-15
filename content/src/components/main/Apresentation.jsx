161a0f3b43afa681aad3ad5a9bb9418d6dbe175c
DanielArrivabene <danielg.arrivabene@gmail.com>
2023-10-15 20:46:19 -0300

    add contacts

--- content/src/components/main/Apresentation.jsx
+++ content/src/components/main/Apresentation.jsx
@@ -1,16 +1,36 @@
 import '../../styles/components/Apresentation.scss';
 
+
+import { BiLogoLinkedin, BiLogoWhatsapp, BiLogoGithub } from 'react-icons/bi';
+
+const contactsList = [
+  { name: 'LinkedIn', href: '#', icon: <BiLogoLinkedin /> },
+  { name: 'Whatsapp', href: '#', icon: <BiLogoWhatsapp /> },
+  { name: 'Github', href: '#', icon: <BiLogoGithub /> },
+];
+
 function Apresentation() {
   return (
     <section
       id='Apresentation'
-      className='vh-100 d-flex 
+      className=' container vh-100 d-flex 
       flex-column
       justify-content-center align-items-center
     '
     >
-      <h1><i>D</i>aniel <i>G</i>. <i>A</i>rrivabene</h1>
-      <h2>Front-end Developer</h2>
+      <h1>
+        <i>D</i>aniel <i>G</i>. <i>A</i>rrivabene
+      </h1>
+      <h2>Frontend Web Developer</h2>
+
+      {contactsList &
+        contactsList.map((contact) => (
+          <ul key={contact.name} className='list-group contacts-list'>
+            <li className='list-group-item'>
+              <a href={contact.href}>{contact.icon}</a>
+            </li>
+          </ul>
+        ))}
     </section>
   );
 }
---
+++ content/src/data/contacts.js
@@ -0,0 +1,7 @@
+import { BiLogoLinkedin, BiLogoWhatsapp, BiLogoGithub } from 'react-icons/bi';
+
+export const contactsList = [
+  { name: 'LinkedIn', href: '#', icon: <BiLogoLinkedin /> },
+  { name: 'Whatsapp', href: '#', icon: <BiLogoWhatsapp /> },
+  { name: 'Github', href: '#', icon: <BiLogoGithub /> },
+];
