cfc0cb90152cd562d1d1879ad71ee124b5b9c10c
DanielArrivabene <danielg.arrivabene@gmail.com>
2023-10-15 20:17:34 -0300

    add Apresentation components

--- content/src/components/main/Apresentation.jsx
+++ content/src/components/main/Apresentation.jsx
@@ -1,8 +1,18 @@
+import '../../styles/components/Apresentation.scss';
 
 function Apresentation() {
   return (
-    <div>Apresentation</div>
-  )
+    <section
+      id='Apresentation'
+      className='vh-100 d-flex 
+      flex-column
+      justify-content-center align-items-center
+    '
+    >
+      <h1><i>D</i>aniel <i>G</i>. <i>A</i>rrivabene</h1>
+      <h2>Front-end Developer</h2>
+    </section>
+  );
 }
 
-export default Apresentation
+export default Apresentation;
