8e7f7dfc507a6a439d3930444c59113514fbd264
DanielArrivabene <danielg.arrivabene@gmail.com>
2023-10-16 16:13:00 -0300

    projetos estilizados

--- content/src/components/main/Projects.jsx
+++ content/src/components/main/Projects.jsx
@@ -11,34 +11,31 @@ function Projects() {
           Confira alguns dos projetos já realizados
         </h2>
         <article className='projects__container'>
-          <div className='row'>
+          <div className='row justify-content-center'>
             <div className='col-5 project__image'>
               <img src={Repara} alt='Repara!' loading='lazy' />
             </div>
-            <div className='col-7 project__description'>
+            <div className='col-6 project__description'>
               <h3>Repara!</h3>
               <p>
                 Projeto construído em HTML, Bootstrap, CSS, Sass, Javascript e
                 React. Com foco em apresentar a empresa ao público alvo e atrair
                 clientes, Repara! conta com uma identidade visual única,
                 intuitiva e dinâmica. Fique por dentro dos detalhes{' '}
-                <a href='#'>visitando o site</a>, e{' '}
+                <a href='#'>visitando o site</a> e{' '}
                 <a href='#'>analisando o código</a>.
               </p>
             </div>
           </div>
-          <div className='row'>
+          <div className='row justify-content-center'>
             <div className='col-5 project__image order-1'>
               <img src={Mentalize} alt='Repara!' loading='lazy' />
             </div>
-            <div className='col-7 project__description text-end'>
+            <div className='col-6 project__description text-end'>
               <h3>Mentalize</h3>
               <p>
-                Desenvolvido usando HTML, Bootstrap, CSS, Sass, Javascript e
-                React. Com foco em apresentar a empresa ao público alvo e atrair
-                clientes, Repara! conta com uma identidade visual única,
-                intuitiva e dinâmica. Fique por dentro dos detalhes{' '}
-                <a href='#'>visitando o site</a>, e{' '}
+                Site da clínica médica Mentalize, desenvolvido com HTML, CSS,
+                Sass, Javascript e React, tendo uma área integrada com os posts do Instagram da clínica. O objetivo do projeto é informar aos pacientes sobre as atividades oferecidas e horários de funcionamento da clínica. Verifique a conclusão desse objetivo <a href='#'>visitando o site</a> e{' '}
                 <a href='#'>analisando o código</a>.
               </p>
             </div>
