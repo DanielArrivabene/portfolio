a9c56616e2634cf3032a9563e54ebb8f007dcafe
DanielArrivabene <danielg.arrivabene@gmail.com>
2023-10-16 12:41:11 -0300

    add about content

--- content/src/components/main/About.jsx
+++ content/src/components/main/About.jsx
@@ -1,7 +1,34 @@
 import '../../styles/components/About.scss';
 
+import Avatar from '../../assets/eu6.jpg';
+
 function About() {
-  return <section id="about" className="vh-100">About</section>;
+  return (
+    <section id='about' className='vh-100 d-flex justify-content-around'>
+      <article className='container d-flex align-items-center'>
+        <div className='row'>
+          <div className='col-md-7 text-center avatar__container'>
+            <img className="avatar" src={Avatar} alt='Daniel Goulart Arrivabene' />
+          </div>
+          <div className='col-md-5 description'>
+            <h2 className='description__title'>
+              Buscando por serviços de desenvolvimento web? Eu posso te ajudar!
+            </h2>
+            <p className='description__text'>
+              Olá, Meu nome é <strong>Daniel Goulart Arrivabene</strong> e sou
+              um desenvolvedor apaixonado por construir aplicativos da web de
+              alto nível e com funcionalidades intuitivas. Atuo transformando
+              ideias em realidade usando soluções criativas, sempre em busca de
+              novas habilidades, ferramentas e conceitos.{' '}
+            </p>
+            <a className='description__link' href='#'>
+              Ver Skills
+            </a>
+          </div>
+        </div>
+      </article>
+    </section>
+  );
 }
 
 export default About;
