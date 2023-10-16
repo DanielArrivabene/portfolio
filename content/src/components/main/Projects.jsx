bfcc025bfc2ebacf5f9893c0498b1c9dbbb79bbb
DanielArrivabene <danielg.arrivabene@gmail.com>
2023-10-16 15:57:19 -0300

    add projects midia

--- content/src/components/main/Projects.jsx
+++ content/src/components/main/Projects.jsx
@@ -1,22 +1,48 @@
 import '../../styles/components/Projects.scss';
 
+import Repara from '../../assets/repara.png';
+import Mentalize from '../../assets/mentalize.png';
+
 function Projects() {
   return (
-    <section id='projects' className="min-vh-100 d-flex align-items-center">
+    <section id='projects' className='min-vh-100 d-flex align-items-center'>
       <div className='container'>
-        <h2 className="text-center">Confira alguns dos projetos já realizados:</h2>
-        <article className="projects__container">
-            <div className="row">
-                <div className="col-6"></div>
-                <div className="col-6">
-                    <h3>Repara!</h3>
-                    <p>Projeto construído em HTML, Bootstrap, CSS, Sass, Javascript e React. Com foco em apresentar a empresa ao público alvo e atrair clientes, Repara! conta com uma identidade visual única e intuitiva. Fique por dentro dos detalhes visitando o site, e analisando o código.</p>
-                </div>
+        <h2 className='text-center'>
+          Confira alguns dos projetos já realizados
+        </h2>
+        <article className='projects__container'>
+          <div className='row'>
+            <div className='col-5 project__image'>
+              <img src={Repara} alt='Repara!' loading='lazy' />
             </div>
-            <div className="row">
-                <div className="col-6 order-1"></div>
-                <div className="col-6">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas esse qui tempora ipsum quod quam nemo unde, corporis dicta cum nam incidunt reprehenderit aspernatur non ad veniam quidem inventore obcaecati?</div>
+            <div className='col-7 project__description'>
+              <h3>Repara!</h3>
+              <p>
+                Projeto construído em HTML, Bootstrap, CSS, Sass, Javascript e
+                React. Com foco em apresentar a empresa ao público alvo e atrair
+                clientes, Repara! conta com uma identidade visual única,
+                intuitiva e dinâmica. Fique por dentro dos detalhes{' '}
+                <a href='#'>visitando o site</a>, e{' '}
+                <a href='#'>analisando o código</a>.
+              </p>
             </div>
+          </div>
+          <div className='row'>
+            <div className='col-5 project__image order-1'>
+              <img src={Mentalize} alt='Repara!' loading='lazy' />
+            </div>
+            <div className='col-7 project__description text-end'>
+              <h3>Mentalize</h3>
+              <p>
+                Desenvolvido usando HTML, Bootstrap, CSS, Sass, Javascript e
+                React. Com foco em apresentar a empresa ao público alvo e atrair
+                clientes, Repara! conta com uma identidade visual única,
+                intuitiva e dinâmica. Fique por dentro dos detalhes{' '}
+                <a href='#'>visitando o site</a>, e{' '}
+                <a href='#'>analisando o código</a>.
+              </p>
+            </div>
+          </div>
         </article>
       </div>
     </section>
