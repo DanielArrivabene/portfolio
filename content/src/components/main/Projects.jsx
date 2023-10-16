f42497fcf5979a09efc365ab82b31705a243fa36
DanielArrivabene <danielg.arrivabene@gmail.com>
2023-10-16 14:57:06 -0300

    projects base ok

--- content/src/components/main/Projects.jsx
+++ content/src/components/main/Projects.jsx
@@ -2,10 +2,22 @@ import '../../styles/components/Projects.scss';
 
 function Projects() {
   return (
-    <section id='projects' className="min-vh-100">
+    <section id='projects' className="min-vh-100 d-flex align-items-center">
       <div className='container'>
-        <h2>Confira alguns dos projetos já realizados:</h2>
-        <article></article>
+        <h2 className="text-center">Confira alguns dos projetos já realizados:</h2>
+        <article className="projects__container">
+            <div className="row">
+                <div className="col-6"></div>
+                <div className="col-6">
+                    <h3>Repara!</h3>
+                    <p>Projeto construído em HTML, Bootstrap, CSS, Sass, Javascript e React. Com foco em apresentar a empresa ao público alvo e atrair clientes, Repara! conta com uma identidade visual única e intuitiva. Fique por dentro dos detalhes visitando o site, e analisando o código.</p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-6 order-1"></div>
+                <div className="col-6">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas esse qui tempora ipsum quod quam nemo unde, corporis dicta cum nam incidunt reprehenderit aspernatur non ad veniam quidem inventore obcaecati?</div>
+            </div>
+        </article>
       </div>
     </section>
   );
--- content/src/data/contacts.js
+++
@@ -1,9 +0,0 @@
-import { BiLogoLinkedin, BiLogoWhatsapp, BiLogoGithub } from 'react-icons/bi';
-
-const contactsList = [
-  { name: 'LinkedIn', href: '#', icon: <BiLogoLinkedin /> },
-  { name: 'Whatsapp', href: '#', icon: <BiLogoWhatsapp /> },
-  { name: 'Github', href: '#', icon: <BiLogoGithub /> },
-];
-
-export default contactsList;
