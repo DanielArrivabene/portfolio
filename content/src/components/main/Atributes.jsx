21f144a2c292ee2f7d2b758e3e9118ab55cd6201
DanielArrivabene <139562422+DanielArrivabene@users.noreply.github.com>
2024-01-24 18:54:27 -0300

    update atributes layout

--- content/src/components/main/Atributes.jsx
+++ content/src/components/main/Atributes.jsx
@@ -1,5 +1,7 @@
 import '../../styles/components/Atributes.scss';
 
+import { motion } from 'framer-motion';
+
 import Ray from '../../assets/ray.svg';
 import Chain from '../../assets/chain.svg';
 import Star from '../../assets/star.svg';
@@ -30,16 +32,46 @@ function Atributes() {
     >
       <article className='container d-flex align-items-center justify-content-center'>
         <div className='w-100 row justify-content-around cards__container'>
-          <h2 className='text-center mb-5'>Feito sob medida<span className="destaque--text">.</span></h2>
+          <h2 className='text-center mb-5'>
+            Feito sob medida<span className='destaque--text'>.</span>
+          </h2>
           {atributesData &&
             atributesData.map((atribute, index) => (
               <div
                 key={index}
-                className='col-lg-4 w text-center atribute__card p-3'
+                className='col-lg-4 w text-center atribute__card p-2'
               >
-              {atribute.icon && <img src={atribute.icon} alt='atributes icons' />}
-                <h3 className='card__title mb-3 mt-5'>{atribute.title}</h3>
-                <p className='card__description'>{atribute.text}</p>
+                {atribute.icon && (
+                  <motion.img
+                    src={atribute.icon}
+                    alt='atributes icons'
+                    initial={{
+                      opacity: 0,
+                    }}
+                    animate={{
+                      opacity: 1,
+                    }}
+                    transition={{
+                      duration: .4,
+                      delay: 2 - index,
+                    }}
+                  />
+                )}
+                <motion.div
+                  initial={{
+                    opacity: 0,
+                  }}
+                  animate={{
+                    opacity: 1,
+                  }}
+                  transition={{
+                    duration: 1,
+                    delay: 2 - index,
+                  }}
+                >
+                  <h3 className='card__title mb-3 mt-3'>{atribute.title}</h3>
+                  <p className='card__description'>{atribute.text}</p>
+                </motion.div>
               </div>
             ))}
         </div>
