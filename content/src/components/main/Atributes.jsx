dc4a7c07296a32d0ea34b0f73af89e303eb05d59
DanielArrivabene <139562422+DanielArrivabene@users.noreply.github.com>
2024-02-16 20:26:40 -0300

    update atributes section

--- content/src/components/main/Atributes.jsx
+++ content/src/components/main/Atributes.jsx
@@ -1,25 +1,25 @@
 import '../../styles/components/Atributes.scss';
 
-import Ray from '../../assets/ray.svg';
-import Chain from '../../assets/chain.svg';
-import Star from '../../assets/star.svg';
+import { GiCoffeeBeans } from 'react-icons/gi';
+import { HiComputerDesktop } from 'react-icons/hi2';
+import { PiCrownFill } from "react-icons/pi";
 
 function Atributes() {
   const atributesData = [
     {
-      title: 'Rápido e fácil',
-      text: 'Rápido e fácil para que você possa otimizar seu tempo, entregando um conteúdo de qualidade ao seu público alvo.',
-      icon: Ray,
+      title: '612+',
+      text: 'Xícaras de café.',
+      icon: <GiCoffeeBeans />,
     },
     {
-      title: 'Rentável',
-      text: 'Rentabilidade é a prioridade, tendo como principal objetivo fazer com que o dinheiro investido gere lucros cada vez maiores.',
-      icon: Chain,
+      title: '50+',
+      text: 'Clientes satisfeitos.',
+      icon: <PiCrownFill />,
     },
     {
-      title: 'Foco nos detalhes',
-      text: 'Pequenos detalhes fazem grande diferença no dia a dia aumentando o engajamento e atraindo novos clientes.',
-      icon: Star,
+      title: '1000+',
+      text: 'Horas codando.',
+      icon: <HiComputerDesktop />,
     },
   ];
 
@@ -29,35 +29,18 @@ function Atributes() {
       className='d-flex justify-content-around section-area'
     >
       <article className='container d-flex align-items-center justify-content-center'>
-        <div className='w-100 row justify-content-around cards__container'>
-          <h2 className='text-center mb-4'>
-            Feito sob medida<span className='destaque--text'>.</span>
-          </h2>
+        <div className='w-100 row justify-content-around cards__container p-3'>
           {atributesData &&
             atributesData.map((atribute, index) => (
               <div
                 key={index}
                 className='col-lg-4 text-center atribute__card p-2'
               >
-                {atribute.icon && (
-                  <img
-                    src={atribute.icon}
-                    loading='lazy'
-                    alt='atributes icons'
-                  />
-                )}
+                {atribute.icon && atribute.icon}
                 <h3 className='card__title mb-3 mt-3'>{atribute.title}</h3>
                 <p className='card__description'>{atribute.text}</p>
               </div>
             ))}
-          <div className='atributes__links_container w-100 d-flex flex-wrap justify-content-center gap-2 mt-3'>
-            <a href='#about' className='atributes__link--about'>
-              Sobre
-            </a>
-            <a href='#projects' className='atributes__link--projects'>
-              Portfólio
-            </a>
-          </div>
         </div>
       </article>
     </section>
