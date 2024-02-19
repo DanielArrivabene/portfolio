d9e48469d0bd15986bc97b58e06f34571dcf750f
DanielArrivabene <139562422+DanielArrivabene@users.noreply.github.com>
2024-02-19 13:27:46 -0300

    update portfólio layout grid

--- content/src/components/main/Projects.jsx
+++ content/src/components/main/Projects.jsx
@@ -1,10 +1,5 @@
 import '../../styles/components/Projects.scss';
 
-import { BiLinkExternal, BiLogoGithub } from 'react-icons/bi';
-
-import Repara from '../../assets/repara.png';
-import Kateli from '../../assets/Kateli.png';
-
 const projectsList = [
   {
     name: 'Clínica Kateli',
@@ -12,19 +7,31 @@ const projectsList = [
     status: 'new',
     description:
       'Projeto construído em WordPress, HTML, CSS e Javascript. Este site conta com um design elegante e refinado, atendendo aos mais altos padrões e gostos.',
-    image: Kateli,
     urlSite: 'http://katelisaude.com.br/',
-    order: 'order-0',
+  },
+  {
+    name: 'Clínica Kateli',
+    class: 'kateli',
+    status: 'new',
+    description:
+      'Projeto construído em WordPress, HTML, CSS e Javascript. Este site conta com um design elegante e refinado, atendendo aos mais altos padrões e gostos.',
+    urlSite: 'http://katelisaude.com.br/',
   },
   {
     name: 'Repara',
     class: 'repara',
     description:
       'Projeto construído em HTML, Bootstrap, CSS, Sass, Javascript e React. Com foco em apresentar a empresa ao público alvo e atrair clientes, Repara! conta com uma identidade visual única, intuitiva e dinâmica.',
-    image: Repara,
-    urlGithub: 'https://github.com/DanielArrivabene/repara',
     urlSite: 'https://repara-tec.vercel.app/',
-    order: 'order-2',
+  },
+  {
+    name: 'Clínica Kateli',
+    class: 'kateli',
+    status: 'new',
+    description:
+      'Projeto construído em WordPress, HTML, CSS e Javascript. Este site conta com um design elegante e refinado, atendendo aos mais altos padrões e gostos.',
+    urlSite: 'http://katelisaude.com.br/',
+    alert: 'Em breve',
   },
 ];
 
@@ -34,42 +41,29 @@ function Projects() {
       id='projects'
       className='min-vh-100 d-flex flex-column align-items-center section-area'
     >
-      <h2 className='text-center'>Portfólio</h2>
-      <p className='text-center mb-5'>
-        Confira alguns dos projetos já realizados.
-      </p>
-      <article className='projects__container'>
-        {projectsList.map((project) => (
-          <div
-            className={`project ${project.order} ${project.class}`}
-            key={project.name}
-          >
-            <div className='project__data p-5'>
-              <h3 className='project__title mb-4'>{project.name}</h3>
-              <div className='project__links_container d-flex gap-3'>
-                <a
-                  href={project.urlSite}
-                  target='_blank'
-                  rel='noreferrer'
-                  className='project__link'
-                >
-                  <BiLinkExternal />
-                </a>
-                {project.urlGithub && (
-                  <a
-                    href={project.urlGithub}
-                    target='_blank'
-                    rel='noreferrer'
-                    className='project__link'
-                  >
-                    <BiLogoGithub />
-                  </a>
-                )}
-              </div>
+      <div className='container'>
+        <h2 className='text-center'>Portfólio</h2>
+        <p className='text-center mb-5'>
+          Confira alguns dos projetos já realizados.
+        </p>
+        <article className='projects__container'>
+          {projectsList.map((project) => (
+            <div className={`project ${project.class}`} key={project.name}>
+              <a
+                href={project.urlSite}
+                target='_blank'
+                rel='noreferrer'
+                className='project__link'
+              >
+                <h3 className='project__title'>{project.name}</h3>
+              </a>
+              {project.alert && (
+                <span className='project__alert'>{project.alert}</span>
+              )}
             </div>
-          </div>
-        ))}
-      </article>
+          ))}
+        </article>
+      </div>
     </section>
   );
 }
