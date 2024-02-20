d00efe258de93a0c2df4ea4cf69bc0bdb94af5b5
DanielArrivabene <139562422+DanielArrivabene@users.noreply.github.com>
2024-02-20 16:04:35 -0300

    update services layout

--- content/src/components/main/Services.jsx
+++ content/src/components/main/Services.jsx
@@ -4,18 +4,22 @@ function Services() {
   const servicesList = [
     {
       title: 'Web Development',
+      subitle: 'MOST POPULAR',
       description: 'Desenvolvimento de sites e sistemas customizados.',
     },
     {
       title: 'UX / UI Design',
+      subitle: 'NEW',
       description: 'Desenvolvimento de sites e sistemas customizados.',
     },
     {
       title: 'Performace Optimization',
+      subitle: 'BUSINESS',
       description: 'Desenvolvimento de sites e sistemas customizados.',
     },
     {
       title: 'Mobile App',
+      subitle: 'COMING SOON',
       description: 'Desenvolvimento de sites e sistemas customizados.',
     },
   ];
@@ -27,13 +31,14 @@ function Services() {
     >
       <article className='container d-flex flex-column align-items-center justify-content-center'>
         <h2 className='mb-3'>Serviços</h2>
-        <p className='fst-italic mb-5'>
+        <p className='fst-italic mb-3'>
           ´´Com grandes responsabilidades vem grandes poderes´´
         </p>
-        <div className='services_container row justify-content-between'>
+        <div className='services_container row justify-content-between mt-3'>
           {servicesList.map((service, index) => (
             <div key={index} className='service p-4'>
-              <h3 className='service__title'>{service.title}</h3>
+              <p className='service__subtitle mb-4'>{service.subitle}</p>
+              <h3 className='service__title mb-2'>{service.title}</h3>
               <p className='m-0 service__description'>{service.description}</p>
               {service.aviso && (
                 <span className='service__aviso'>{service.aviso}</span>
