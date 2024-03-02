a75795a6ecd477582428da75308d2b3b487ebcc3
DanielArrivabene <139562422+DanielArrivabene@users.noreply.github.com>
2024-03-01 21:57:42 -0300

    update hero image

--- content/src/components/main/About.jsx
+++ content/src/components/main/About.jsx
@@ -7,12 +7,12 @@ function About() {
       className='d-flex justify-content-around section-area min-vh-100'
     >
       <article className='container d-flex flex-column align-items-center justify-content-center'>
-        <p className='description__text mb-3'>
-          Me chamo <span className='destaque--text'>Daniel Goulart</span>,
-          estou a mais de 3 anos transformando ideias em realidade. Tudo através de código e uma boa xícara de
-          café.
+        <p className='description__text mb-4'>
+          Me chamo <span className='destaque--text'>Daniel Goulart</span>, estou
+          a mais de 3 anos transformando ideias em realidade. Tudo através de
+          código e uma boa xícara de café.
         </p>
-        <div className='about__links_container'>
+        <div className='about__links_container mt-1'>
           <a className='about__link--one' href='#services'>
             Entrar em Contato
           </a>
