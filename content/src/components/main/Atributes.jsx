0f767dd6c71d6772ad084180bc0144ab5eafa7b4
DanielArrivabene <139562422+DanielArrivabene@users.noreply.github.com>
2024-03-08 19:58:03 -0300

    update atributes

--- content/src/components/main/Atributes.jsx
+++ content/src/components/main/Atributes.jsx
@@ -1,27 +1,23 @@
 import '../../styles/components/Atributes.scss';
 
-import { BsGraphUpArrow, BsArrowsAngleExpand } from "react-icons/bs";
-import { PiClockCountdown } from "react-icons/pi";
-
 function Atributes() {
-
   return (
     <section
       id='atributes'
       className='d-flex justify-content-around section-area'
     >
       <article className='container row justify-content-between align-items-center'>
-        <div className='col-lg-6 atributes__container px-4 d-flex flex-wrap gap-4'>
+        <div className='col-lg-6 atributes__container d-flex flex-wrap gap-4'>
           <div className='atribute one p-4 d-flex justify-content-between align-items-center'>
-            <p className='atribute__service m-0'>
-            <BsGraphUpArrow /> Escalável
-            </p>
-            <p className='atribute__service m-0'>
-            <BsArrowsAngleExpand /> Responsivo
-            </p>
-            <p className='atribute__service m-0'>
-            <PiClockCountdown /> Rápido
-            </p>
+            <h3 className='atribute__service m-0'>
+              Escalável
+            </h3>
+            <h3 className='atribute__service m-0'>
+              Responsivo
+            </h3>
+            <h3 className='atribute__service m-0'>
+              Rápido
+            </h3>
           </div>
           <div className='atribute two p-4'>
             <h3>
@@ -29,30 +25,31 @@ function Atributes() {
               genérico top Título genérico top Título genérico top
             </h3>
           </div>
-          <div className='atribute tree p-4'>
+          <div className='atribute tree p-4 d-flex flex-column justify-content-between'>
             <p className='mb-2'>
               ´´Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Deleniti, quae vero, perferendis ipsum quos totam aperiam´´
+              <strong className='mt-2 d-block'>Por Patrick Mendes</strong>
             </p>
-            <p className='atribute_author'>Por Patrick Mendes</p>
             <a href='#testemonials' className='atribute__link'>
               Ver mais
             </a>
           </div>
         </div>
-        <div className='col-lg-6 atributes__data p-4'>
-          <p className='atributes__text mb-0'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-            accusamus deserunt aperiam quas
-          </p>
-          <h2>Um pouco sobre o que faço</h2>
+        <div className='col-lg-6 atributes__data'>
+          <h2 className='mb-3'>Por quê usar meu serviço</h2>
           <p className='atributes__text mb-3'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
             accusamus deserunt aperiam quas
           </p>
-          <a href='#' className='atributes__link mb-5'>
+          <p className='atributes__text mb-4'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+            accusamus deserunt aperiam quas
+          </p>
+          <a href='#' className='atributes__link mb-2'>
             Saiba mais
           </a>
+          <p className="atributes_call">*Faça um orçamento, é grátis!</p>
         </div>
       </article>
     </section>
