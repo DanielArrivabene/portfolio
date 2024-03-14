fb35ed92dafff4ce3577b22c87b8dd26d7d2eaed
DanielArrivabene <139562422+DanielArrivabene@users.noreply.github.com>
2024-03-14 13:58:23 -0300

    add services icons

--- content/src/components/main/Services.jsx
+++ content/src/components/main/Services.jsx
@@ -1,6 +1,8 @@
 import '../../styles/components/Services.scss';
 
 import { HiArrowLongRight } from 'react-icons/hi2';
+import { BsCodeSlash } from "react-icons/bs";
+import { PiShootingStarLight, PiStarFour } from "react-icons/pi";
 
 function Services() {
   const servicesList = [
@@ -8,30 +10,29 @@ function Services() {
       title: 'Web Dev',
       description:
         'Desenvolvimento de sites sob medida e totalmente personalizados.',
-      icon: '01',
+      icon: <BsCodeSlash />,
     },
     {
       title: 'Sob medida',
       description:
         'Cada site é desenvolvido unicamente para a solução que você precisa.',
-      icon: '02',
+      icon: <PiStarFour />,
     },
     {
       title: 'Com SEO',
       description:
         'Alta visibilidade e rankeamento no Google, fazendo mais pessoas virem até você.',
-      icon: '03',
+      icon: <PiShootingStarLight />,
     },
   ];
 
   return (
     <section id='services' className='d-flex flex-column align-items-center'>
-      <p className='mb-4 services__title'>A VISIBILIDADE QUE VOCÊ MERECE</p>
       <article className='services__container container d-flex justify-content-between'>
         {servicesList.map((service, index) => (
           <div key={index} className='service p-4'>
             <div className='service__data_container'>
-              <h3 className='service__title mb-4'>{service.title}</h3>
+              <h3 className='service__title mb-4'>{service.title} {service.icon}</h3>
               <p className='service__description mb-4'>{service.description}</p>
               <a href='#contact' className='service__link'>
                 Saiba mais <HiArrowLongRight />
