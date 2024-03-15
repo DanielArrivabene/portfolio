f77ba4a61afdde17ce4efc3bc7423dd035f4e397
DanielArrivabene <139562422+DanielArrivabene@users.noreply.github.com>
2024-03-15 14:22:10 -0300

    update about image

--- content/src/components/main/About.jsx
+++ content/src/components/main/About.jsx
@@ -2,6 +2,8 @@ import '../../styles/components/About.scss';
 
 import { MdCheckBox } from 'react-icons/md';
 
+import Avatar from '../../assets/eu.jpg';
+
 function About() {
   const aboutAtributesList = [
     {
@@ -23,10 +25,13 @@ function About() {
       <article className='container row'>
         <div className='col-lg-6 d-flex flex-column justify-content-between about__data'>
           <div className='about__text_container'>
-            <p className="color--primary mb-2">QUEM SOU?</p>
+            <p className='color--primary mb-2'>QUEM SOU?</p>
             <h2 className='mb-4'>A mente por trás da máscara</h2>
             <p className='about__text  mb-3'>
-              Sou um desenvolvedor Web, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi beatae nobis eligendi quam odit, dolore labore perferendis vel deserunt dolorem accusamus aperiam reprehenderit id sunt ad incidunt! Saepe, ut quaerat.
+              Sou um desenvolvedor Web, Lorem ipsum dolor sit amet consectetur,
+              adipisicing elit. Sequi beatae nobis eligendi quam odit, dolore
+              labore perferendis vel deserunt dolorem accusamus aperiam
+              reprehenderit id sunt ad incidunt! Saepe, ut quaerat.
             </p>
             <p className='about__text mb-4'>
               Ter um site permite que <strong>clientes em potencial</strong>{' '}
@@ -49,7 +54,13 @@ function About() {
             ))}
           </ul>
         </div>
-        <div className='col-lg-6 about__image'></div>
+        <div className='col-lg-6 about__image_container'>
+          <img
+            src={Avatar}
+            alt='Daniel Goulart Arrivabene'
+            className='about__image'
+          />
+        </div>
       </article>
     </section>
   );
