f692aea7d7353ceabbcc04c66fa9553d3f5b525a
DanielArrivabene <139562422+DanielArrivabene@users.noreply.github.com>
2024-03-15 16:19:32 -0300

    ok

--- content/src/components/main/Ideal.jsx
+++ content/src/components/main/Ideal.jsx
@@ -10,27 +10,20 @@ function Ideal() {
         <h2 className='ideal_title mb-4'>Seu site ideal</h2>
         <div className='ideal_container row flex-wrap w-100 mb-4'>
           <div className='col-lg-6 p-4'>
-            <p className='ideal__text  mb-3'>
-              Sem um site profissional sua empresa é <strong>invisível</strong>{' '}
-              para milhares de pessoas que buscam pelo seu serviço todos os dias
-              na internet.
-            </p>
-            <p className='ideal__text mb-5'>
+            <p className='ideal__text mb-3'>
               Ter um site permite que <strong>clientes em potencial</strong>{' '}
               encontrem sua empresa facilmente ao procurar por serviços
               relacionados, além de passar uma imagem de{' '}
-              <strong>credibilidade</strong> para sua empresa. Os clientes
-              tendem a confiar mais em empresas que têm uma presença online
-              estabelecida.
+              <strong>credibilidade</strong>. Os clientes tendem a confiar mais
+              em empresas que têm uma presença online estabelecida.
             </p>
-            <h3 className='mb-3'>O que você ganha com isso?</h3>
-            <p className='ideal__text'>
-              De acordo com os dados da <a href='#'>Target Group Index</a> , os
-              acessos via celulares representam cerca de{' '}
-              <strong>64% entre os usuários</strong>.
+            <p className='ideal__text  mb-5'>
+              Sem um site profissional sua empresa é invisível para milhares de
+              pessoas que buscam pelo seu serviço{' '}
+              <strong>todos os dias na internet.</strong>
             </p>
+            <h3 className='mb-3'>Como escolher o site ideal?</h3>
             <p className='ideal__text'>
-              {' '}
               De acordo com a{' '}
               <a
                 target='_blank'
@@ -38,12 +31,15 @@ function Ideal() {
                 href='https://hoodzpahdesign.com/'
               >
                 Hoodzpah Design
-              </a>
-              , 54% dos usuários de internet se sentem frustrados ao acessar um
-              site que não é <strong>responsivo</strong>. E{' '}
-              <strong>52% dos usuários</strong> disseram que seriam menos
-              propensos a se envolver com uma empresa caso a experiência com seu
-              website não fosse boa.
+              </a>{' '}
+              52% dos usuários disseram que seriam menos propensos a se envolver
+              com uma empresa caso a experiência com seu website não fosse boa.
+            </p>
+            <p className='ideal__text'>
+              O processo de desenvolvimento conta com várias estapas, e sem
+              dúvidas durante todas o foco na <strong>sua necessidade</strong>,
+              sendo assim, para alcançar o resultado esperado é essencial a
+              colaboração e
             </p>
             <a href='#atributes' className='ideal__link mb-4'>
               Veja as vantagens
