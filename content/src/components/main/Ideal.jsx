835f9497c1b9dcb9bc1b166e4f3acc393e83e2e5
DanielArrivabene <139562422+DanielArrivabene@users.noreply.github.com>
2024-03-18 13:18:47 -0300

    add ideal mq

--- content/src/components/main/Ideal.jsx
+++ content/src/components/main/Ideal.jsx
@@ -8,7 +8,7 @@ function Ideal() {
       <article className='container d-flex flex-column align-items-center'>
         <p className='color--primary mb-1'>EM BUSCA DO</p>
         <h2 className='ideal_title mb-4'>Seu site ideal</h2>
-        <div className='ideal_container row flex-wrap w-100 mb-4'>
+        <div className='ideal_container row flex-wrap-reverse w-100'>
           <div className='col-lg-6 p-4'>
             <p className='ideal__text mb-3'>
               Ter um site permite que <strong>clientes em potencial</strong>{' '}
@@ -35,18 +35,18 @@ function Ideal() {
               52% dos usuários disseram que seriam menos propensos a se envolver
               com uma empresa caso a experiência com seu website não fosse boa.
             </p>
-            <p className='ideal__text'>
+            <p className='ideal__text mb-4'>
               O processo de desenvolvimento conta com várias estapas, e sem
               dúvidas durante todas o foco na <strong>sua necessidade</strong>,
               sendo assim, para alcançar o resultado esperado é essencial a
               colaboração e
             </p>
-            <a href='#atributes' className='ideal__link mb-4'>
+            <a href='#atributes' className='ideal__link'>
               Veja as vantagens
             </a>
           </div>
           <div className='ideal__image_container col-lg-6 p-4'>
-            <img className='ideal__image' src={Image} alt='' />
+            <img className='ideal__image mb-5' src={Image} alt='' />
           </div>
         </div>
       </article>
